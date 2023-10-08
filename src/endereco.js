f0fb5abbfbf7dca3d4395d87c2a13aadb2601ccb
Bros12Lek <123905721+Bros12Lek@users.noreply.github.com>
2023-10-08 14:57:57 +0000

    Funcionalidades do endereço feitas[adriano]

--- src/endereco.js
+++ src/endereco.js
@@ -1,4 +1,8 @@
 const cep = document.querySelector("#cep");
+const lograd = document.querySelector("#lograd");
+const compl = document.querySelector("#compl");
+const bai = document.querySelector("#bai");
+const cidade = document.querySelector("#cidade");
 
 cep.addEventListener("blur" , (event) =>{
     fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
@@ -7,5 +11,10 @@ cep.addEventListener("blur" , (event) =>{
         })
         .then((Response) =>{
             console.log(Response)
+            const {logradouro, complemento,bairro,localidade} = Response;
+            lograd.value = logradouro;
+            compl.value = complemento
+            bai.value = bairro
+            cidade.value = localidade;
         })
 })
